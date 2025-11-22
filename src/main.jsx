6384889f100208573851abd35d017756dfa7d9d2
NamanSh <snaman9967@gmail.com>
2025-11-22 14:05:42 +0530

    hashrouter relaod issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>
 
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-    </BrowserRouter>
 ,
 )
